51242d5fab36f57251d2b034707d763fed25b4a5
David "DJ" Jennings <davidjennings@gmail.com>
2019-11-12 22:06:34 -0800

    Add files via upload
    Initial implementation of 3 sort types

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+package com.davidjennings.selfstudy.sorting;
+
+public class BubbleSort extends SortBase
+{
+	int[] sort(int[] input)
+	{
+		if (input.length <=1) return input;
+		int n = input.length;
+
+		// Outer loop over all elements in the input. The right half of the array will hold the
+		// highest sorted values, growing to the left until all are sorted.
+		for (int i=1; i < n; i++)
+		{
+			boolean swapped = false;
+
+			// The inner "bubbling" loop walks all elements from first and up to but not including
+			// first sorted element, swapping out of order values along the way. Final result of each
+			// inner loop is the highest value in the elements up to n-1 ends up stored at n-1.
+			for (int j=0; j<n-1; j++)
+			{
+				if (isGreaterThan(input, j, j+1))
+				{
+					swapped = true;
+					swap(input, j, j + 1);
+				}
+			}
+
+			// Optimization to end if any full pass discovers the list is fully sorted. This gives
+			// the algorithm O(N) time complexity when the list is already sorted.
+			if (!swapped)
+				break;
+		}
+
+		return input;
+	}
+
+	public static void main(String[] args)
+	{
+		new InsertionSort().tryTestCases();
+	}
+}
---
+++ sorting/SortBase.java
@@ -0,0 +1,72 @@
+package com.davidjennings.selfstudy.sorting;
+
+import java.util.Arrays;
+
+public abstract class SortBase
+{
+	private int numSwaps, numCompares;
+
+	abstract int[] sort(int[] input);
+
+	void swap(int[] array, int i1, int i2)
+	{
+		int tmp = array[i1];
+		array[i1] = array[i2];
+		array[i2] = tmp;
+		numSwaps++;
+	}
+
+	boolean isLessThan(int[] array, int i1, int i2)
+	{
+		numCompares++;
+		return array[i1] < array[i2];
+	}
+
+	boolean isGreaterThan(int[] array, int i1, int i2)
+	{
+		numCompares++;
+		return array[i1] > array[i2];
+	}
+
+	private String toString(int[] input)
+	{
+		StringBuilder result = new StringBuilder("[");
+		for (int i=0; i< input.length; i++)
+		{
+			if (i != input.length-1)
+				result.append(input[i]).append(", ");
+			else
+				result.append(input[i]).append("]");
+		}
+
+		return result.toString();
+	}
+
+	public void tryTestCase(int[] input, String msg)
+	{
+		numSwaps = 0;
+		numCompares = 0;
+
+		System.out.println(String.format("==> %s", msg));
+		int[] check = Arrays.copyOf(input, input.length);
+		Arrays.sort(check);
+
+		System.out.println(String.format("Input:   %s", toString(input)));
+
+		int[] sorted = sort(input);
+		System.out.println(String.format("Sorted:  %s", toString(sorted)));
+
+		if (Arrays.equals(check, sorted))
+			System.out.println(String.format("Correct! (%d compares + %d swaps)", numCompares, numSwaps));
+		else
+			System.out.println("FAIL! It's not sorted.");
+
+		System.out.println();
+	}
+
+	public void tryTestCases()
+	{
+		tryTestCase(new int[]{17, 4, 5, 1, 22, 3, 8, 4, 21}, "Mixed data");
+		tryTestCase(new int[]{1, 3, 4, 4, 5, 8, 17, 21, 22}, "Already sorted data");
+	}
+}
